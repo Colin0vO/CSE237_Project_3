410de86589789ccd25228a7bcb7d2cf16a675b58
Zhijun Wang <zhw106@ucsd.edu>
2025-11-07 23:48:25 -0800

    Add files via upload

---
+++ Q5/dft.cpp
@@ -0,0 +1,47 @@
+#include "dft.h"
+#include "coefficients32_2D.h"
+
+
+
+void dft(DTYPE XX_R[SIZE], DTYPE XX_I[SIZE])
+{
+    DTYPE out_real[SIZE];
+    DTYPE out_imag[SIZE];
+
+#pragma HLS ARRAY_PARTITION variable=XX_R     block factor=PART_FACTOR dim=1
+#pragma HLS ARRAY_PARTITION variable=XX_I     block factor=PART_FACTOR dim=1
+#pragma HLS ARRAY_PARTITION variable=out_real block factor=PART_FACTOR dim=1
+#pragma HLS ARRAY_PARTITION variable=out_imag block factor=PART_FACTOR dim=1
+
+#pragma HLS ARRAY_PARTITION variable=cos_coeff_table block factor=PART_FACTOR dim=2
+#pragma HLS ARRAY_PARTITION variable=sin_coeff_table block factor=PART_FACTOR dim=2
+
+loop_k:
+    for (int k = 0; k < SIZE; k++) {
+        DTYPE sum_real = 0.0f;
+        DTYPE sum_imag = 0.0f;
+
+    loop_n:
+        for (int n = 0; n < SIZE; n++) {
+#pragma HLS UNROLL factor=UNROLL_FACTOR
+            DTYPE xr = XX_R[n];
+            DTYPE xi = XX_I[n];
+
+            DTYPE c = cos_coeff_table[k][n];
+            DTYPE s = sin_coeff_table[k][n];
+
+            // (xr + j*xi) * (c + j*s)
+            sum_real += xr * c - xi * s;
+            sum_imag += xr * s + xi * c;
+        }
+
+        out_real[k] = sum_real;
+        out_imag[k] = sum_imag;
+    }
+
+write_back:
+    for (int k = 0; k < SIZE; k++) {
+        XX_R[k] = out_real[k];
+        XX_I[k] = out_imag[k];
+    }
+}
---
+++ Q5/dft.h
@@ -0,0 +1,15 @@
+#ifndef DFT_H
+#define DFT_H
+
+typedef float DTYPE;
+#define SIZE 32
+
+
+#define PART_FACTOR   16
+
+
+#define UNROLL_FACTOR 32
+
+void dft(DTYPE XX_R[SIZE], DTYPE XX_I[SIZE]);
+
+#endif
